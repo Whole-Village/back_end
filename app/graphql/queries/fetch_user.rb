edea4cd5616c25be6ffd7aca5828a83ae64cf806
iPowers <isika.powers@gmail.com>
2021-12-04 16:14:41 -0700

    Create users type and mutation

--- app/graphql/queries/fetch_user.rb
+++
@@ -1,6 +0,0 @@
-module Queries
-  class FetchUser < Queries::BaseQuery
-    type [Types::UserType], null: false
-    
-  end
-end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,12 +4,16 @@ module Types
     field :first_name, String, null: true
     field :last_name, String, null: true
     field :email, String, null: true
-    field :password_digest, String, null: true
+    field :password, String, null: true
     field :phone_number, String, null: true
     field :address, String, null: true
     field :covid_vaccinated, Boolean, null: true
     field :volunteer_credits, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def full_name
+      [object.first_name, object.last_name].compact.join(" ")
+    end
   end
 end
