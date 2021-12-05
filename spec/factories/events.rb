43df6ee3c79b864ff351469e8e332997f98b625b
Andrew Massey <acmassey@ncsu.edu>
2021-12-04 21:17:29 -0500

    Create Event model

---
+++ spec/factories/events.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :event do
+    name { "MyString" }
+    date { "MyString" }
+    description { "MyString" }
+    time { "MyString" }
+    adult_required { false }
+    images { "MyString" }
+  end
+end
