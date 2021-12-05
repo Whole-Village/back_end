316f29aaabec7a40eb21470686d1bc058fa1df5c
Andrew Massey <acmassey@ncsu.edu>
2021-12-04 22:21:09 -0500

    Add dependent destroy

--- app/models/village.rb
+++ app/models/village.rb
@@ -1,6 +1,6 @@
 class Village < ApplicationRecord
-  has_many :village_members
-  has_many :village_events
+  has_many :village_members, dependent: :destroy
+  has_many :village_events, dependent: :destroy
   has_many :users, through: :village_members
   has_many :events, through: :village_events
 
