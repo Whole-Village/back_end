0f9e2fa5fcec94f81de4f69a117298b154c19aa8
Andrew Massey <acmassey@ncsu.edu>
2021-12-04 22:21:18 -0500

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Child.destroy_all
+VillageMember.destroy_all
+VillageEvent.destroy_all
+Event.destroy_all
+Village.destroy_all
+
 
 donna =  User.create(first_name: 'Donna',
               last_name: 'Doodoo',
@@ -35,4 +42,43 @@ priya = User.create(first_name: 'Priya',
              volunteer_credits: 0
               )
 
-              
+andrew = Child.create(
+  first_name: 'Andrew',
+  last_name: 'Apples',
+  birthdate: '03/06/1998',
+  user_id: priya.id
+)
+
+natalie = Child.create(
+  first_name: 'Natalie',
+  last_name: 'Nabisco',
+  birthdate: '01/15/2000',
+  user_id: amber.id
+)
+
+village1 = Village.create(
+  name: 'Soccer team'
+)
+
+vm1 = VillageMember.create(
+  village: village1,
+  user: priya
+)
+
+vm2 = VillageMember.create(
+  village: village1,
+  user: amber
+)
+
+event1 = Event.create(
+  name: 'Andrews birthday bash',
+  date: '03/06/1998',
+  description: 'come throw down for Andrew',
+  time: '04:30',
+  adult_required: false,
+)
+
+ve1 = VillageEvent.create(
+  village: village1,
+  event: event1
+)
