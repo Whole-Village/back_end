59797858ed9a05d3493f9144c8a9499d8224fdef
Andrew Massey <acmassey@ncsu.edu>
2021-12-06 13:23:21 -0500

    Construct mutation for creating a village

---
+++ app/graphql/mutations/create_village.rb
@@ -0,0 +1,25 @@
+class Mutations::CreateVillage < Mutations::BaseMutation
+  argument :name, String, required: true
+  argument :description, String, required:false
+
+  field :village, Types::VillageType, null: true
+  field :errors, [String], null: false
+
+  def resolve(name:,
+              description:)
+
+    village = Village.new(name: name,
+                          description: description)
+    if village.save
+      {
+        village: village,
+        errors: []
+      }
+    else
+      {
+        village: nil,
+        errors: village.errros.full_messages
+      }
+    end
+  end
+end
