ff9ce0f179697ab2b71d9e211fd38ca5257f359d
iPowers <isika.powers@gmail.com>
2021-12-06 13:16:37 -0700

    Create user and users queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,22 +1,6 @@
-module Types
-  class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-    # field :users, resolver: Queries::Users
-    # field :user, resolver: Queries::FetchUser
-    field :users, [Types::UserType], null: false,
-      description: 'Fetch all users'
-
-    def users
-      User.all.order(created_at: :desc)
-    end
-
-    field :user, [Types::UserType], null: false do
-      argument :email, String, required: true
-    end
-
-    def user(email:)
-      User.find_by(email: email)
-    end
-  end
+class Types::QueryType < Types::BaseObject
+  # Add root-level fields here.
+  # They will be entry points for queries on your schema.
+  field :users, resolver: Queries::Users
+  field :user, resolver: Queries::User
 end
