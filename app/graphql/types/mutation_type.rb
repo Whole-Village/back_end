77772c0c4ed8e4ba6b33f93f7e4b62258057ce07
iPowers <isika.powers@gmail.com>
2021-12-08 12:19:49 -0700

    Create create and update event mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,9 @@
 class Types::MutationType < Types::BaseObject
-  field :create_user, mutation: Mutations::CreateUser
-  field :update_user, mutation: Mutations::UpdateUser
+  field :create_user, mutation: Mutations::Users::CreateUser
+  field :update_user, mutation: Mutations::Users::UpdateUser
+
   field :create_village, mutation: Mutations::CreateVillage
+
+  field :create_event, mutation: Mutations::Events::CreateEvent
+  field :update_event, mutation: Mutations::Events::UpdateEvent
 end
