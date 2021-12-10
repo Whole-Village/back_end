c752c4e0bdf5ab13b3bf6a225307236e31847b26
Andrew Massey <acmassey@ncsu.edu>
2021-12-10 17:16:50 -0500

    Add users and events field on the village query

--- app/graphql/types/village_type.rb
+++ app/graphql/types/village_type.rb
@@ -3,6 +3,8 @@ module Types
     field :id, ID, null: false
     field :name, String, null: true
     field :description, String, null: true
+    field :users, [Types::UserType], null: true
+    field :events, [Types::EventType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
