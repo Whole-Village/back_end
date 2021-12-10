9b82e4aa1c26bb72ac993f7ad9986713f0e6e3bb
Andrew Massey <acmassey@ncsu.edu>
2021-12-10 18:41:54 -0500

    Change mutation so it takes the user email and not the user ID

--- app/graphql/mutations/village_members/create_village_member.rb
+++ app/graphql/mutations/village_members/create_village_member.rb
@@ -1,14 +1,14 @@
 class Mutations::VillageMembers::CreateVillageMember < ::Mutations::BaseMutation
-  argument :village_id, ID, required: false
-  argument :user_id, ID, required: false
+  argument :village_id, ID, required: true
+  argument :user_email, String, required: true
 
   field :village, Types::VillageType, null: true
   field :user, Types::UserType, null: true
   field :errors, [String], null: true
 
-  def resolve(village_id:, user_id:)
+  def resolve(village_id:, user_email:)
     member = VillageMember.new(village: Village.find(village_id),
-                               user: User.find(user_id)
+                               user: User.find_by(email: user_email)
     )
     if member.save
       {
