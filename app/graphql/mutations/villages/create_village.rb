21b30c252559f76be20db4572b971f41be1486aa
Andrew Massey <acmassey@ncsu.edu>
2021-12-11 19:34:29 -0500

    Update the create village mutation so it takes an array of user emails and creates those members

--- app/graphql/mutations/villages/create_village.rb
+++ app/graphql/mutations/villages/create_village.rb
@@ -1,16 +1,20 @@
 class Mutations::Villages::CreateVillage < Mutations::BaseMutation
   argument :name, String, required: true
-  argument :description, String, required:false
+  argument :description, String, required: false
+  argument :user_emails, [String], required: false
 
   field :village, Types::VillageType, null: true
   field :errors, [String], null: false
 
   def resolve(name:,
-              description:)
-
+              description:, user_emails:)
     village = Village.new(name: name,
                           description: description)
+
     if village.save
+      user_emails.each do |u|
+        village.village_members.create(user: User.find_by(email: u))
+      end
       {
         village: village,
         errors: []
