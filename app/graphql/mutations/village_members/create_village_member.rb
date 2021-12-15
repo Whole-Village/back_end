0eb3fb5b6068ff0968b92310cb743bfebd9d1a93
Andrew Massey <acmassey@ncsu.edu>
2021-12-15 10:46:09 -0500

    Add error handling for village member creation

--- app/graphql/mutations/village_members/create_village_member.rb
+++ app/graphql/mutations/village_members/create_village_member.rb
@@ -7,9 +7,9 @@ class Mutations::VillageMembers::CreateVillageMember < ::Mutations::BaseMutation
   field :errors, [String], null: true
 
   def resolve(village_id:, user_email:)
-    member = VillageMember.new(village: Village.find(village_id),
-                               user: User.find_by(email: user_email)
-    )
+    user = User.find_by(email: user_email)
+    village = Village.find(village_id)
+    member = VillageMember.new(village: village, user: user)
     if member.save
       {
         village: member.village,
@@ -22,5 +22,10 @@ class Mutations::VillageMembers::CreateVillageMember < ::Mutations::BaseMutation
         errors: member.errors.full_messages
       }
     end
+    rescue ActiveRecord::RecordNotFound => _e
+      GraphQL::ExecutionError.new('User or Village does not exist.')
+    rescue ActiveRecord::RecordInvalid => e
+      GraphQL::ExecutionError.new("Invalid attribute(s) for #{e.record.class}:"\
+      " #{e.record.errors.full_messages.join(', ')}")
   end
 end
